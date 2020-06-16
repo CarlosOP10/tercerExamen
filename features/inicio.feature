fc8a442219bf04964c9e812d73c79926da33a88f
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2020-06-16 09:39:12 -0400

    Se agrego la primera prueba de aceptacion, mas falta implementarle

---
+++ features/inicio.feature
@@ -0,0 +1,8 @@
+Feature:
+Como usuario
+Quiero ingresar a la pagina de inicio
+Para realizar mis transacciones
+
+Scenario:
+Given visito la pagina principal
+Then observo el mensaje "El balance de tu cuenta es:"
---
+++ features/step_definitions/inicio_steps.rb
@@ -0,0 +1,7 @@
+Given('visito la pagina principal') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then('observo el mensaje {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
