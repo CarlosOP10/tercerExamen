2076bab838a442b6f53fbaec35fad3dfcf5d1ba9
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2020-06-16 09:49:40 -0400

    se puso en marcha el ingresarMonto.feature

---
+++ features/ingresarMonto.feature
@@ -0,0 +1,9 @@
+Feature:
+Como usuario
+Quiero ingresar a la pagina de inicio
+Para ingresar un monto y realizar una transaccion
+
+Scenario:
+Given visito la pagina principal
+And ingreso el 20 en el campo "monto"
+Then hago click en el boton "Depositar"
---
+++ features/step_definitions/ingresarMonto_steps.rb
@@ -0,0 +1,7 @@
+Given('ingreso el {float} en el campo {string}') do |float, string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('hago click en el boton {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
